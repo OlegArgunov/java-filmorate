dd2d995d88e63da0023b96ce79858284fb35a296
OlegArgunov <olegargunov@yandex.ru>
2024-02-22 11:19:03 +0300

    fix: 3 реализовать ТЗ Спринт 9

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,9 +1,10 @@
 package ru.yandex.practicum.filmorate.exceptions;
-import java.lang.RuntimeException;
-public class ValidationException extends Exception{
+
+public class ValidationException extends Exception {
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public ValidationException(String message) {
         super(message);
     }
