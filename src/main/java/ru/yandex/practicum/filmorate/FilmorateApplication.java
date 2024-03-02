ce21d09b42f077ac68d3dfee4a85e5ad07f510c3
OlegArgunov <olegargunov@yandex.ru>
2024-03-02 13:42:02 +0300

    fix: 4 устранить замечания ТЗ Спринт 10

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.UserController;
 
 @SpringBootApplication
 public class FilmorateApplication {
